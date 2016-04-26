e8699ad38a0175dd99db9a491b5f5679c6e7b224
dylbop <dylangreif@dylans-mbp.risd.edu>
2016-04-26 13:20:54 -0400

    new site pages for today

--- pages/04-19/html-prototype-2/script.js
+++ pages/04-19/html-prototype-2/script.js
@@ -2,15 +2,9 @@ function toggleDesktop() {
     if (document.getElementById("frame").className == "outer-frame") {
         document.getElementById("frame").className = "outer-frame desktop";
     }
-    else {
-        document.getElementById("frame").className == "outer-frame";
-    }
 }
 function toggleMobile() {
     if (document.getElementById("frame").className == "outer-frame desktop") {
         document.getElementById("frame").className = "outer-frame";
-    }
-    else {
-        document.getElementById("frame").className == "outer-frame desktop";
     }
 }
