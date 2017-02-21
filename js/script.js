24adab4653711bfd47ae8bff66d36edb3b10b761
dylbop <dylangreif@dylans-mbp-2.risd.edu>
2017-02-21 12:22:21 -0500

    updated for class today 1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function collapseDesc() {
+    if (  $( '#desc.collapsible' ).hasClass( 'expand' ) ) {
+        $( '#desc.collapsible' ).removeClass( 'expand' );
+    } else {
+        $( '#desc.collapsible' ).addClass( 'expand' );
+    }
+}
+function collapseRead() {
+    if (  $( '#read.collapsible' ).hasClass( 'expand' ) ) {
+        $( '#read.collapsible' ).removeClass( 'expand' );
+    } else {
+        $( '#read.collapsible' ).addClass( 'expand' );
+    }
+}
+function collapseProj() {
+    if (  $( '#proj.collapsible' ).hasClass( 'expand' ) ) {
+        $( '#proj.collapsible' ).removeClass( 'expand' );
+    } else {
+        $( '#proj.collapsible' ).addClass( 'expand' );
+    }
+}
